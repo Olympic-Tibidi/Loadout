495d9c20ccc93f99ef66cfdcae66c287a8ce6629
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-07-25 15:23:27 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -306,26 +306,30 @@ with tab3:
 
 
 
-    image = camera_input_live()
-
-    if image is not None:
-        st.image(image)
-        bytes_data = image.getvalue()
-        cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-
-        
-        bd = cv2.barcode.BarcodeDetector()
-        #bd = cv2.barcode.BarcodeDetector('path/to/sr.prototxt', 'path/to/sr.caffemodel')
-
-        retval, data, decoded_type, points = bd.detectAndDecode(img)    
-        #data, bbox, straight_qrcode = detector.detectAndDecode(cv2_img)
+    camera_id = 0
+    delay = 1
+    window_name = 'OpenCV Barcode'
     
-        if data:
-            st.write("# Found QR code")
-            st.write(data)
-            with st.expander("Show details"):
-                #st.write("BBox:", bbox)
-                st.write("Straight QR code:", data)
+    bd = cv2.barcode.BarcodeDetector()
+    cap = cv2.VideoCapture(camera_id)
+    
+    while True:
+        ret, frame = cap.read()
+    
+        if ret:
+            ret_bc, decoded_info, _, points = bd.detectAndDecode(frame)
+            if ret_bc:
+                frame = cv2.polylines(frame, points.astype(int), True, (0, 255, 0), 3)
+                for s, p in zip(decoded_info, points):
+                    if s:
+                        print(s)
+                        frame = cv2.putText(frame, s, p[1].astype(int),
+                                            cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2, cv2.LINE_AA)
+            cv2.imshow(window_name, frame)
+    
+        if cv2.waitKey(delay) & 0xFF == ord('q'):
+            break
+
 #     st.markdown("**QR Code Reader**")
 #     
 #     #st.selectbox("SUBMIT FOR LOAD",[f"LOAD-{i}" for i in range(1,11)],key="for_capture")
