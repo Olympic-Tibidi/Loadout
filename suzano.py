18c638f31b487e5d51efc01fd7d6b632ec2599ed
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-08-07 13:41:37 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -621,8 +621,9 @@ if select=="INVENTORY" :
         filtered_zf=zf.copy()
         if date_filter:
             filtered_zf["Warehouse_Out"]=[i.date() for i in filtered_zf["Warehouse_Out"]]
-            st.write(filtered_zf)
+            
             filtered_zf=filtered_zf[filtered_zf["Warehouse_Out"]==filter_date]
+            st.write(filtered_zf)
         dryweight_filter=st.selectbox("Filter By DryWeight",["ALL DRYWEIGHTS"]+[str(i) for i in filtered_zf["DryWeight"].unique().tolist()])
         BL_filter=st.selectbox("Filter By Bill Of Lading",["ALL BILL OF LADINGS"]+[str(i) for i in filtered_zf["BL"].unique().tolist()])
         vehicle_filter=st.selectbox("Filter By Vehicle_Id",["ALL VEHICLES"]+[str(i) for i in filtered_zf["Vehicle_Id"].unique().tolist()])
