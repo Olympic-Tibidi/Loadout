9ab5f1f31d7bce0e140ef34e7ffb58306f2bb387
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-08-08 12:14:20 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -395,6 +395,7 @@ if select=="LOADOUT" :
 
     current=gcp_download("olym_suzano","dispatched.json")
     current=json.loads(current)
+    st.write(current)
     info=gcp_download("olym_suzano",rf"release_orders/{current['vessel']}/{current['release_order']}.json")
     info=json.loads(info)
     #st.write(info)
