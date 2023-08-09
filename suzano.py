706ebc617a4a1ea8fb8c5a3a323523afdceca611
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-08-09 12:33:44 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -620,7 +620,7 @@ if select=="LOADOUT" :
             send_email_with_attachment(subject, body, sender, recipients, password, file_path,file_name)
             upload_cs_file("olym_suzano", 'temp_file.txt',file_name) 
     except:
-        st.markdown(f"**red:[NO RELEASE ORDERS DISPATCHED YET!!/n PLEASE CHYECK BACK SOON]**")
+        st.markdown(f"**:red[NO RELEASE ORDERS DISPATCHED YET!!/n PLEASE CHYECK BACK SOON]**")
     
             
     
