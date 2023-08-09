11666085c122bf97f08e59c23153513c5c1ebc32
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-08-09 14:13:21 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -473,7 +473,7 @@ if select=="LOADOUT" :
                 
         def audit_unit(x):
             if len(x)==11:
-                st.write(bill_mapping[x[:-3]]["Batch"])
+                #st.write(bill_mapping[x[:-3]]["Batch"])
                 if bill_mapping[x[:-3]]["Ocean_bl"]!=ocean_bill_of_lading and bill_mapping[x[:-3]]["Batch"]!=batch:
                     st.write("WRONG UNIT, scan another one")
                 else:
