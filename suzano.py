abad6f36a87d4eb99bd6e5a6a47cb74bce540016
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-08-11 07:53:32 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -550,13 +550,15 @@ if select=="LOADOUT" :
             st.markdown(rf'**Remaining : {info[vessel][current_release_order][current_sales_order]["remaining"]}**')
             
         with load_col2:
-            try:
-                st.markdown(rf'**NEXT ITEM : Release Order-{next_release_order}**')
-                st.markdown(rf'**Sales Order Item-{next_sales_order}**')
-                st.markdown(f'**Ocean Bill Of Lading : {info[vessel][next_release_order][next_sales_order]["ocean_bill_of_lading"]}**')
-                st.markdown(rf'**Total Quantity : {info[vessel][next_release_order][next_sales_order]["quantity"]}**')
-            except:
-                pass
+            if double_load:
+                
+                try:
+                    st.markdown(rf'**NEXT ITEM : Release Order-{next_release_order}**')
+                    st.markdown(rf'**Sales Order Item-{next_sales_order}**')
+                    st.markdown(f'**Ocean Bill Of Lading : {info[vessel][next_release_order][next_sales_order]["ocean_bill_of_lading"]}**')
+                    st.markdown(rf'**Total Quantity : {info[vessel][next_release_order][next_sales_order]["quantity"]}**')
+                except:
+                    pass
               
         col1, col2,col3,col4,col5= st.columns([2,2,2,2,2])
         
