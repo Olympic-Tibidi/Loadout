b38bc3ce22322ea46b10de3fe8e7667fc29a8d1c
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-08-13 08:25:16 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -699,7 +699,7 @@ if select=="LOADOUT" :
                 bucket = storage_client.bucket("olym_suzano")
                 blob = bucket.blob(rf"terminal_bill_of_ladings.json")
                 blob.upload_from_string(bill_of_ladings)
-            terminal_bill_of_lading=st.text_input("Terminal Bill of Lading",st.session_state.number,disabled=True)
+            terminal_bill_of_lading=st.text_input("Terminal Bill of Lading",st.session_state.number,disabled=False)
        
         with col4:
             updated_quantity=0
