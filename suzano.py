a80052e456d2ecd165ca3fb80e3ea92e233eed54
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-08-14 15:09:26 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -750,10 +750,10 @@ if check_password():
                 def audit_unit(x):
                         if len(x)==11:
                             #st.write(bill_mapping[x[:-3]]["Batch"])
-                            
+                            st.write(Inventory_Audit[Inventory_Audit["Lot"]==x]["Location"])
                             if bill_mapping[x[:-3]]["Ocean_bl"]!=ocean_bill_of_lading and bill_mapping[x[:-3]]["Batch"]!=batch:
                                 st.write("WRONG B/L, DO NOT LOAD!")
-                            st.write(Inventory_Audit[Inventory_Audit["Lot"]==x]["Location"])
+                            
                             #if Inventory_Audit[Inventory_Audit["Lot"]==x]["Location"]!="OLYM":
                                # st.write("THIS UNIT HAS BEEN SHIPPED")
                             
