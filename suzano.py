0b60b0a16d3f7c41fdfad48e3b3f4a31e520a75d
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-08-19 22:17:54 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -785,7 +785,7 @@ if gty==1:
                         current_release_order=work_order
                         current_sales_order=i
                         vessel=dispatched[work_order][i]["vessel"]
-                        destination=info[vessel][work_order]['destination']
+                        destination==dispatched[work_order][i]['destination']
                     else:
                         pass
                 try:
