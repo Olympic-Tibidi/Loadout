d6b02455272b9c2fa7efe7664cde36ca2401b302
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-08-19 17:55:11 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -1167,9 +1167,10 @@ if gty==1:
                         filename = f'{a}{b}OLYM'
                         file_name= f'{a}{b}OLYM.txt'
                         st.write(filename)
+                        st.write(current_release_order,current_sales_order,destination,ocean_bill_of_lading,terminal_bill_of_lading,wrap)
                         subject = f'Suzano_EDI_{a}_{release_order_number}'
                         body = f"EDI for Below attached.{newline}  Release Order Number : {current_release_order} - Sales Order Number:{current_sales_order}{newline}   Destination : {destination} Ocean Bill Of Lading : {ocean_bill_of_lading}{newline}                  Terminal Bill of Lading: {terminal_bill_of_lading} - Wrap : {wrap} {newline}  {len(loads)} {unitized} loads were loaded to vehicle : {vehicle_id} with Carried ID : {carrier_id} {newline}    Truck loading comnpleted at {a} {b}"
-                                   
+                        st.write(body)           
                         sender = "warehouseoly@gmail.com"
                         #recipients = ["alexandras@portolympia.com","conleyb@portolympia.com", "afsiny@portolympia.com"]
                         recipients = ["afsiny@portolympia.com"]
