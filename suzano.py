7d53e793fd44fda7553796536c34cb5440bd4b43
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-08-20 19:34:51 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -1279,7 +1279,7 @@ if authentication_status:
                             if estimated_arrival>now:
                                 st.write(f"Truck No : {truck} is Enroute to {destination} with ETA {estimated_arrival_string}")
                                 enroute_vehicles[truck]={"DESTINATION":destination,"CARGO":bill_of_ladings[i]["ocean_bill_of_lading"],
-                                                 "QUANTITY":f'{bill_of_ladings[i]["quantity"]}',"LOADED TIME":f"{ship_date}---{ship_time}","ETA":estimated_arrival_string}
+                                                 "QUANTITY":f'{2*bill_of_ladings[i]["quantity"]} TONS',"LOADED TIME":f"{ship_date}---{ship_time}","ETA":estimated_arrival_string}
                             else:
                                 with daily3:
                                     st.write(f"Truck No : {truck} arrived at {destination} at {estimated_arrival_string}")
