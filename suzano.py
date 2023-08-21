adec01654a0f3d4b41bcab97b587582b302b67c9
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-08-21 11:02:02 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -325,7 +325,7 @@ if authentication_status:
                 requested_edi=gcp_download("olym_suzano", rf"EDIS/KIRKENES-2304/{requested_edi_file}")
                 
                
-                st.text_area("EDI",requested_edi,height=800)
+                st.text_area("EDI",requested_edi,height=400)
                
                                                                                  
             with admin_tab5:
