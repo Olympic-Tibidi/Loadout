2655734d8b3535619d187b209f04e9682a0dd752
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-08-21 14:34:54 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -1442,7 +1442,7 @@ if authentication_status:
                                       ],
                                      'threshold' : {'line': {'color': "red", 'width': 4}, 'thickness': 0.75, 'value': 1500}}))
 
-                    fig.show()
+                    st.plotly_chart(fig)
 
 
 
