fc6e8fd8018f00db2f1a2b509a5db8062ca60bda
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-08-21 22:24:27 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -1372,6 +1372,7 @@ if authentication_status:
                     suzano_report_=gcp_download("olym_suzano",rf"suzano_report.json")
                     suzano_report=json.loads(suzano_report_)
                     suzano_report=pd.DataFrame(suzano_report).T
+                    suzano_report=suzano_report[["Date Shipped","Vehicle", "Shipment ID #", "Consignee","Consignee City","Consignee State","Release #","Carrier","ETA","Ocean BOL#","Warehouse","Vessel","Voyage #","Grade","Quantity","Metric Ton", "ADMT","Mode of Transportation"]]
                     st.dataframe(suzano_report)
                 except:
                     st.write("NO REPORTS RECORDED")
