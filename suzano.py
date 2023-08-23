54abc562f9b687037ac372ff93a268417d0aebfa
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-08-22 21:12:44 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -1409,7 +1409,7 @@ if authentication_status:
                     
                 
                     st.download_button(
-                        label="DOWNLOAD REPOLRT AS CSV",
+                        label="DOWNLOAD REPORT AS CSV",
                         data=csv,
                         file_name=f'OLYMPIA_DAILY_REPORT{datetime.datetime.strftime(datetime.datetime.now()-datetime.timedelta(hours=7),"%Y_%m_%d")}.csv',
                         mime='text/csv')
@@ -2153,7 +2153,7 @@ if authentication_status:
                 
                 
                 st.download_button(
-                    label="DOWNLOAD REPOLRT AS CSV",
+                    label="DOWNLOAD REPORT AS CSV",
                     data=csv,
                     file_name=f'OLYMPIA_DAILY_REPORT{datetime.datetime.strftime(datetime.datetime.now()-datetime.timedelta(hours=7),"%Y_%m_%d")}.csv',
                     mime='text/csv')
