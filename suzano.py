6465d01e2b9f06f97c085290d55883fe255451b7
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-08-22 21:44:36 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -1590,7 +1590,7 @@ if authentication_status:
                             ),
                             xaxis=dict(tickfont=dict(size=10)),  # Increase x-axis tick label font size
                             yaxis=dict(tickfont=dict(size=12)),  # Increase y-axis tick label font size
-                            title_font=dict(size=16, weight="bold")  # Increase title font size and weight
+                            title_font=dict(size=16)  # Increase title font size and weight
                         )
 
                 st.plotly_chart(fig)
