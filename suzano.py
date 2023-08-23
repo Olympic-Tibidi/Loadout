6c188b13fae026a25280b2db1ee67fa2ed53367d
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-08-22 20:51:04 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -525,8 +525,9 @@ if authentication_status:
                         
                         for key in release_order_database:
                             for sales in key:
-                                if release_order_database[key][sales]["remaining"]<=0:
-                                    completed_release_orders.append(key)
+                                if release_order_database[key][sales]["remaining"]>0:
+                                    break
+                            st.write(f"{key} seems to be finished")
                         
                         files_in_folder_ = [i.replace(".json","") for i in list_files_in_subfolder("olym_suzano", rf"release_orders/KIRKENES-2304/")]
                         files_in_folder=[i for i in files_in_folder_ if i not in completed_release_orders]
