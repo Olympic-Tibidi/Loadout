ddd98d15aa919865f88f2c590b7e5a033d3ab013
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-08-26 12:14:27 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -941,12 +941,12 @@ if authentication_status:
                     vehicle_id=st.text_input("**:blue[Vehicle ID]**",value="",key=7)
                     foreman_quantity=st.number_input("**:blue[ENTER Quantity of Units]**", min_value=0, max_value=30, value=0, step=1, help=None, on_change=None, disabled=False, label_visibility="visible",key=8)
                     click_clear1 = st.button('CLEAR VEHICLE-QUANTITY INPUTS', key=34)
-                        if click_clear1:
-                           carrier_code=placeholder.text_input("Carrier Code",info[vessel][current_release_order][current_sales_order]["carrier_code"],disabled=True,key=9)
-                           transport_sequential_number=placeholder.selectbox("Transport Sequential",["TRUCK","RAIL"],disabled=True,key=10)
-                           transport_type=placeholder.selectbox("Transport Type",["TRUCK","RAIL"],disabled=True,key=11)
-                           vehicle_id=placeholder.text_input("**:blue[Vehicle ID]**",value="",key=12)
-                           foreman_quantity=placeholder.number_input("**:blue[ENTER Quantity of Units]**", min_value=0, max_value=30, value=0, step=1, help=None, on_change=None, disabled=False, label_visibility="visible",key=13)
+                    if click_clear1:
+                       carrier_code=placeholder.text_input("Carrier Code",info[vessel][current_release_order][current_sales_order]["carrier_code"],disabled=True,key=9)
+                       transport_sequential_number=placeholder.selectbox("Transport Sequential",["TRUCK","RAIL"],disabled=True,key=10)
+                       transport_type=placeholder.selectbox("Transport Type",["TRUCK","RAIL"],disabled=True,key=11)
+                       vehicle_id=placeholder.text_input("**:blue[Vehicle ID]**",value="",key=12)
+                       foreman_quantity=placeholder.number_input("**:blue[ENTER Quantity of Units]**", min_value=0, max_value=30, value=0, step=1, help=None, on_change=None, disabled=False, label_visibility="visible",key=13)
 
 
 
