90471faa6205eb2d9a67895642ed65c1face9c98
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-09-06 12:36:31 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -1183,7 +1183,7 @@ if authentication_status:
                                         st.write(f"**Unit No : {i+1}**",x)
                                         faults.append(0)
                                 else:
-                                    st.markdown(f"**:red[Unit No : {i+1}]-{x}**"unsafe_allow_html=True)
+                                    st.markdown(f"**:red[Unit No : {i+1}]-{x}**",unsafe_allow_html=True)
                                     faults.append(1)
                                 seen.add(x)
                         if bale_load_input is not None:
