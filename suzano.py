e0b03827c1287ff5c496b87e7e5d47d2a4773919
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-09-06 10:53:08 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -551,12 +551,15 @@ if authentication_status:
                         files_in_folder_ = [i.replace(".json","") for i in list_files_in_subfolder("olym_suzano", rf"release_orders/KIRKENES-2304/")]   ### REMOVE json extension from name
                         files_in_folder=[i for i in files_in_folder_ if i not in completed_release_orders]        ###  CHECK IF COMPLETED
                         release_order_dest_map={}
-                        for i in release_order_dictionary:
-                            for sales in release_order_dictionary[i]:
-                                release_order_dest_map[i]=release_order_dictionary[i][sales]["destination"]
-                        
-                        destinations_of_release_orders=[f"{i} to {release_order_dest_map[i]}" for i in files_in_folder]
-                        
+                        try:
+                            
+                            for i in release_order_dictionary:
+                                for sales in release_order_dictionary[i]:
+                                    release_order_dest_map[i]=release_order_dictionary[i][sales]["destination"]
+                            
+                            destinations_of_release_orders=[f"{i} to {release_order_dest_map[i]}" for i in files_in_folder]
+                        except:
+                            st.write("NO RELEASE ORDERS YET")
                                                                         
                         requested_file_=st.selectbox("ACTIVE RELEASE ORDERS",destinations_of_release_orders)
                         requested_file=requested_file_.split(" ")[0]
