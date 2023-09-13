feb084616bb5fb6cf97146180176caecd4929b22
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-09-13 10:09:18 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -339,6 +339,7 @@ if authentication_status:
                     st.write(i)
                     release_bank.append(json.loads(gcp_download("olym_suzano",  rf"release_orders/KIRKENES-2304/{i}")))
                 release_database=json.loads(gcp_download("olym_suzano",  rf"release_orders/RELEASE_ORDERS.json"))
+                release_database_j=json.dumps(release_database)
                 
                 bill_of_ladings=json.loads(gcp_download("olym_suzano",  rf"terminal_bill_of_ladings.json"))
                 bill_of_lading_j=json.dumps(bill_of_ladings)
@@ -346,132 +347,19 @@ if authentication_status:
                 mill_progress_j=gcp_download("olym_suzano",  rf"mill_progress.json")
                 inv=gcp_csv_to_df("olym_suzano", "Inventory.csv")
                 invj=inv.to_csv("invj")
+                files=[edis_bank,release_bank,bill_of_lading_j,release_database_j]
                 
+                def merge_JsonFiles(filename):
+                    result = list()
+                    for f1 in filename:
+                        with open(f1, 'r') as infile:
+                            result.extend(json.load(infile))
                 
+                    with open('counseling3.json', 'w') as output_file:
+                        json.dump(result, output_file)
+
+                merge_JsonFiles(files)
                 
-                config = {
-                    "color": "red",
-                    "amount": 42.24,
-                    "steps": 30,
-                    "version": "1.0",
-                    "widgets": ["Red Widget", "Blue Widget", "Green Widget"],
-                    "timestamp": "2020-07-12T12:32:21Z",
-                }
-                
-                def data(config):
-                    val = config["amount"]
-                    yield {"time": 0, "value": val}
-                    for i in range(1, config["steps"]+1):
-                        s = random.random() * 5
-                        if random.random() < 0.5:
-                            s *= -1
-                        val += s
-                        yield {"time": i, "value": val}
-                
-                
-                def make_archive_bad(path="test.zip"):
-                    # This doesn't work
-                    with zipfile.ZipFile(path, "x") as z:
-                        with z.open("config.json", "w") as c:
-                            print(type(c))
-                            json.dump(config, c, indent=2)
-                
-                        with z.open("data.json", "w") as d:
-                            for row in data(config):
-                                d.write(json.dumps(row))
-                                d.write("\n")
-                
-                
-                def make_archive_annoying(path="test.zip"):
-                    # This does work but is annoying
-                    # Also note, this will write to the root of the archive, and when unzipped will not
-                    # unzip to a directory but rather into the same directory as the zip file
-                    with zipfile.ZipFile(path, "x") as z:
-                        with z.open("config.json", "w") as c:
-                            c.write(json.dumps(config, indent=2).encode("utf-8"))
-                
-                        with z.open("data.json", "w") as d:
-                            for row in data(config):
-                                d.write(json.dumps(row).encode("utf-8"))
-                                d.write("\n".encode("utf-8"))
-                
-                
-                class Zipr(object):
-                
-                    def __enter__(self):
-                        # Makes this thing a context manager
-                        return self
-                
-                    def __exit__(self, exc_type, exc_value, traceback):
-                        # Makes this thing a context manager
-                        self.close()
-                
-                    def close(self):
-                        self.zobj.close()
-                
-                
-                class ZipArchive(Zipr):
-                
-                    def __init__(self, path, mode="r"):
-                        self.zobj = zipfile.ZipFile(
-                            path, mode, compression=zipfile.ZIP_STORED,
-                            allowZip64=True, compresslevel=None
-                        )
-                        self.root, _ = os.path.splitext(os.path.basename(path))
-                
-                    def open(self, path, mode='r'):
-                        # Write into a directory instead of the root of the zip file
-                        path = os.path.join(self.root, path)
-                        return ZipArchiveFile(self.zobj.open(path, mode))
-                
-                
-                class ZipArchiveFile(Zipr):
-                
-                    def __init__(self, zobj, encoding="utf-8"):
-                        self.zobj = zobj
-                        self.encoding = encoding
-                
-                    def write(self, data):
-                        if isinstance(data, str):
-                            data = data.encode(self.encoding)
-                        self.zobj.write(data)
-                
-                
-                def make_archive(path="test.zip"):
-                    # Less annoying make archive with workaround classes
-                    with ZipArchive(path, "x") as z:
-                        with z.open("config.json", "w") as c:
-                            json.dump(config, c, indent=2)
-                
-                        with z.open("data.json", "w") as d:
-                            for row in data(config):
-                                d.write(json.dumps(row))
-                                d.write("\n")
-                
-                
-                def make_archive_stream(path="test.zip"):
-                    # Attempts to open the internal zip file for appending, to stream data in.
-                    # But this doesn't work because you can't open an internal zip object for appending
-                    with ZipArchive(path, "x") as z:
-                        with z.open("config.json", "w") as c:
-                            json.dump(config, c, indent=2)
-                
-                        cache = []
-                        for i, row in enumerate(data(config)):
-                            cache.append(json.dumps(row))
-                
-                            # dump cache every 5 rows
-                            if i%5 == 0:
-                                with z.open("data.json", "a") as d:
-                                    for row in cache:
-                                        d.write(row+"\n")
-                                cache = []
-                
-                        if len(cache) > 0:
-                            with z.open("data.json", "a") as d:
-                                for row in cache:
-                                    d.write(row+"\n")
-                make_archive()
             
                 st.text_area("RELEASE ORDER INDEX",release_database)
                 st.text_area("EDI",edis_bank)
