11caad4bf20c8f004b86e6db8e7d47659d3f4484
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-09-13 11:06:03 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -495,7 +495,7 @@ if authentication_status:
                         else:
                             
                             temp=store_release_order_data(vessel,release_order_number,destination,po_number,sales_order_item,batch,ocean_bill_of_lading,wrap,dryness,unitized,quantity,tonnage,transport_type,carrier_code)
-                            #st.write(temp)
+                     
                         try:
                             junk=gcp_download("olym_suzano",rf"release_orders/{vessel}/junk_release.json")
                         except:
@@ -772,16 +772,16 @@ if authentication_status:
                                     blob = bucket.blob(rf"release_orders/{vessel}/{requested_file}.json")
                                     blob.upload_from_string(json_data)
                                 if st.button("DELETE RELEASE ORDER ITEM!",key="laladg"):
-                                    #junk=gcp_download("olym_suzano",rf"release_orders/{vessel}/junk_release.json")
-                                    #junk=json.loads(junk)
+                                    junk=gcp_download("olym_suzano",rf"release_orders/{vessel}/junk_release.json")
+                                    junk=json.loads(junk)
                                     pass
                                     #st.write(to_edit_d)
-                                    #junk[requested_file]=datetime.datetime.strftime(datetime.datetime.today(),"%Y-%m-%d")
-                                   #json_data = json.dumps(junk)
-                                   # storage_client = storage.Client()
-                                   # bucket = storage_client.bucket("olym_suzano")
-                                   # blob = bucket.blob(rf"release_orders/{vessel}/junk_release.json")
-                                   # blob.upload_from_string(json_data)
+                                    junk[requested_file]=1
+                                    json_data = json.dumps(junk)
+                                    storage_client = storage.Client()
+                                    bucket = storage_client.bucket("olym_suzano")
+                                    blob = bucket.blob(rf"release_orders/{vessel}/junk_release.json")
+                                    blob.upload_from_string(json_data)
                                            
                             with dol2:  
                                 if st.button("CLEAR DISPATCH QUEUE!"):
