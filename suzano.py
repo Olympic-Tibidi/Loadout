1b181b3ab8680df119f3dc93549e03a74221c1f9
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-09-13 09:56:04 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -344,9 +344,7 @@ if authentication_status:
                 mill_progress=json.loads(gcp_download("olym_suzano",  rf"mill_progress.json"))
                 inv=gcp_csv_to_df("olym_suzano", "Inventory.csv")
                 invj=inv.to_csv("invj")
-                b64 = base64.b64encode(invj.encode()).decode()
-                href = f'<a href="data:file/invj(;base64,{b64}" download="all.csv">** ⯈ Download all results.**</a>'
-                st.markdown(href, unsafe_allow_html=True)
+                
                 
                 
                 zipObj = ZipFile("sample.zip", "w")
