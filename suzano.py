6fc146aac9f985a7acf52fd90e95ff01d86b8d98
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-11-06 23:07:34 -0800

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -2028,6 +2028,18 @@ if authentication_status:
                     transport_sequential_number=st.selectbox("Transport Sequential",["TRUCK","RAIL"],disabled=True,key=51)
                     transport_type=st.selectbox("Transport Type",["TRUCK","RAIL"],disabled=True,key=6)
                     vehicle_id=st.text_input("**:blue[Vehicle ID]**",value="",key=7)
+                    mf=True
+                    load_mf_number_issued=False
+                    if carrier_code=="123456-KBX":
+                       if release_order_number in mf_numbers_for_load[vessel].keys():
+                           mf_liste=[i for i in mf_numbers_for_load[vessel][release_order_number]]
+                           load_mf_number=st.selectbox("MF NUMBER",mf_liste,disabled=False,key=14551)
+                           mf=True
+                           load_mf_number_issued=True
+                    else:
+                       st.write("MF NUMBERS NOT IN!")
+                       mf=False
+                       load_mf_number_issued=False  
                     foreman_quantity=st.number_input("**:blue[ENTER Quantity of Units]**", min_value=0, max_value=30, value=0, step=1, help=None, on_change=None, disabled=False, label_visibility="visible",key=8)
                     foreman_bale_quantity=st.number_input("**:blue[ENTER Quantity of Bales]**", min_value=0, max_value=30, value=0, step=1, help=None, on_change=None, disabled=False, label_visibility="visible",key=123)
 
@@ -2039,18 +2051,7 @@ if authentication_status:
                        transport_sequential_number=st.selectbox("Transport Sequential",["TRUCK","RAIL"],disabled=True,key=10)
                        transport_type=st.selectbox("Transport Type",["TRUCK","RAIL"],disabled=True,key=11)
                        vehicle_id=st.text_input("**:blue[Vehicle ID]**",value="",key=12)
-                       mf=True
-                       load_mf_number_issued=False
-                       if carrier_code=="123456-KBX":
-                           if release_order_number in mf_numbers_for_load[vessel].keys():
-                               mf_liste=[i for i in mf_numbers_for_load[vessel][release_order_number]]
-                               load_mf_number=st.selectbox("MF NUMBER",mf_liste,disabled=False,key=14551)
-                               mf=True
-                               load_mf_number_issued=True
-                       else:
-                           st.write("MF NUMBERS NOT IN!")
-                           mf=False
-                           load_mf_number_issued=False  
+                       
                        foreman_quantity=st.number_input("**:blue[ENTER Quantity of Units]**", min_value=0, max_value=30, value=0, step=1, help=None, on_change=None, disabled=False, label_visibility="visible",key=193)
                        foreman_bale_quantity=st.number_input("**:blue[ENTER Quantity of Bales]**", min_value=0, max_value=30, value=0, step=1, help=None, on_change=None, disabled=False, label_visibility="visible",key=13)
 
