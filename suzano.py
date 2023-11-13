6da01ea5592eeafa9a53bedb3c4172989c15549b
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-11-13 10:27:36 -0800

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -1805,7 +1805,7 @@ if authentication_status:
                                     kirkenes_updated.loc[kirkenes_updated["Lot"]==unit[:-2],"Remaining"]=kirkenes_updated.loc[kirkenes_updated["Lot"]==unit[:-2],"Remaining"]-line[unit]*8
                             
                             st.write(kirkenes_updated)
-                            temp=new_df.to_csv("temp.csv")
+                            temp=kirkenes_updated.to_csv("temp.csv")
                             upload_cs_file(target_bucket, 'temp.csv',rf"kirkenes_updated.csv") 
                         no_of_unaccounted=Inventory[Inventory["Accounted"]==False]["Bales"].sum()/8
                         st.write(f'**Unaccounted Units Registered : {no_of_unaccounted} Units/{no_of_unaccounted*2} Tons**')
