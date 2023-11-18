dca6b62d3384de678f0154c9ad81464a28cdc149
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-11-18 13:47:36 -0800

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -590,7 +590,7 @@ if authentication_status:
                     with lab_col7:
                         st.markdown("**WAGES**")
                         with lab_col4:
-                            st.subheader(f"     TOTAL COST FOR SHIFT: $ {round(sum([key['cost'] for key in ranks]),2)}")
+                            st.subheader(f"     TOTAL COST FOR SHIFT: $ {round(sum([ranks[key]['cost'] for key in ranks]),2)}")
                             
                         
                         
