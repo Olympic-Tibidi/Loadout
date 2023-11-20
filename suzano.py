607852aed4d49aa265c497aff76f6c2142cef3ec
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-11-20 12:21:00 -0800

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -347,52 +347,55 @@ if authentication_status:
             
         if select=="DATA BACKUP" :
             st.write(datetime.datetime.now()-datetime.timedelta(hours=utc_difference))
-            scorecard = pd.DataFrame(columns=['User', 'Hour', 'Ot', 'Totaled'])
-
-            # Input your data using experimental data editor
-            st.write("Input your data below:")
-            input_data = pd.DataFrame(index=[1,2,3,4,5], columns=['Rank', 'Shift', 'Hour','Ot'])
-            input_data = input_data.fillna(0)  # fill with zeros
-            
-            edited_data = st.experimental_data_editor(input_data)
-            
-            # Handle user input
-            if st.button('Submit'):
-                edited_data['Totaled'] = edited_data['Hour'] + edited_data['Ot']
-                scorecard = scorecard.append(edited_data, ignore_index=True)
-            
-            # Display the updated scorecard
-            st.write("Updated Scorecard:")
-            st.table(scorecard)
-            if "scores" not in st.session_state:
-                st.session_state.scores = [
-                    {"name": "Josh", "Pushups": 10, "Situps": 20},
-                ]
-            
-            
-            def new_scores():
-                st.session_state.scores.append(
-                    {
-                        "name": st.session_state.name,
-                        "Pushups": st.session_state.pushups,
-                        "Situps": st.session_state.situps,
-                    }
-                )
-            
-            
-            st.write("# Score table")
-            
-            score_df = pd.DataFrame(st.session_state.scores)
-            score_df["total_points"] = score_df["Pushups"] + score_df["Situps"]
-            
-            st.write(score_df)
-            
-            st.write("# Add a new score")
-            with st.form("new_score", clear_on_submit=True):
-                name = st.text_input("Name", key="name")
-                pushups = st.number_input("Pushups", key="pushups", step=1, value=0, min_value=0)
-                situps = st.number_input("Situps", key="situps", step=1, value=0, min_value=0)
-                st.form_submit_button("Submit", on_click=new_scores)
+            try_lan=False
+            if try_lan:
+                
+                scorecard = pd.DataFrame(columns=['User', 'Hour', 'Ot', 'Totaled'])
+    
+                # Input your data using experimental data editor
+                st.write("Input your data below:")
+                input_data = pd.DataFrame(index=[1,2,3,4,5], columns=['Rank', 'Shift', 'Hour','Ot'])
+                input_data = input_data.fillna(0)  # fill with zeros
+                
+                edited_data = st.experimental_data_editor(input_data)
+                
+                # Handle user input
+                if st.button('Submit'):
+                    edited_data['Totaled'] = edited_data['Hour'] + edited_data['Ot']
+                    scorecard = scorecard.append(edited_data, ignore_index=True)
+                
+                # Display the updated scorecard
+                st.write("Updated Scorecard:")
+                st.table(scorecard)
+                if "scores" not in st.session_state:
+                    st.session_state.scores = [
+                        {"name": "Josh", "Pushups": 10, "Situps": 20},
+                    ]
+                
+                
+                def new_scores():
+                    st.session_state.scores.append(
+                        {
+                            "name": st.session_state.name,
+                            "Pushups": st.session_state.pushups,
+                            "Situps": st.session_state.situps,
+                        }
+                    )
+                
+                
+                st.write("# Score table")
+                
+                score_df = pd.DataFrame(st.session_state.scores)
+                score_df["total_points"] = score_df["Pushups"] + score_df["Situps"]
+                
+                st.write(score_df)
+                
+                st.write("# Add a new score")
+                with st.form("new_score", clear_on_submit=True):
+                    name = st.text_input("Name", key="name")
+                    pushups = st.number_input("Pushups", key="pushups", step=1, value=0, min_value=0)
+                    situps = st.number_input("Situps", key="situps", step=1, value=0, min_value=0)
+                    st.form_submit_button("Submit", on_click=new_scores)
             def download_files_in_folder(bucket, folder_name, output_directory):
                 blob_iterator = bucket.list_blobs(prefix=folder_name)
             
