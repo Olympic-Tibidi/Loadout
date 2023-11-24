e22c18bdb985ff9300dcd8a176840319a771a93e
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-11-24 13:14:07 -0800

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -586,7 +586,7 @@ if authentication_status:
                             with sub_col3:
                                 template_choice_valid=False
                                 template_choice=st.selectbox("Select Recorded Template",[i for i in list_files_in_subfolder(target_bucket, rf"labor_templates/")],
-                                                             placeholder="Pick From List", disabled=False, label_visibility="collapsed")
+                                                              label_visibility="collapsed")
                                 if template_choice!="Pick From List":
                                     template_choice_valid=True 
                                 if template_choice_valid:
