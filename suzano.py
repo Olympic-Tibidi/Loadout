0c5a8e5c4596de6c48c0bd0eb9ee9608ebdb810f
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-12-13 11:46:45 -0800

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -2329,9 +2329,8 @@ if authentication_status:
                         ###       Dropdown menu
                         nofile=0
                         requested_file_=st.selectbox("ACTIVE RELEASE ORDERS",destinations_of_release_orders)
-                        st.write(requested_file_)
                         requested_file=requested_file_.split(" ")[0]
-                        st.write(requested_file)
+                 
                         
                        
                         data=gcp_download(target_bucket,rf"release_orders/ORDERS/{requested_file}.json")
