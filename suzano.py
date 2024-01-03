b439a5034d4fc9f2082148590ae394b94c3c0595
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-01-03 15:16:12 -0800

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -5014,8 +5014,10 @@ if authentication_status:
                 inv4tab1,inv4tab2,inv4tab3=st.tabs(["DAILY SHIPMENT REPORT","INVENTORY","UNREGISTERED LOTS FOUND"])
                 with inv4tab1:
                     
+                    inv_vessel=st.selectbox("Select Vessel",["KIRKENES-2304","JUVENTAS-2308"])
                     kf=inv_bill_of_ladings.iloc[1:].copy()
                     kf['issued'] = pd.to_datetime(kf['issued'])
+                    kf=kf[kf['vessel']==inv_vessel]
                     kf['Date'] = kf['issued'].dt.date
                     kf['Date'] = pd.to_datetime(kf['Date'])
                     # Create a date range from the minimum to maximum date in the 'issued' column
