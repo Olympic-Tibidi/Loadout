275056f7b501d81a69c013b7b32a0bb35849508e
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-02-01 09:50:10 -0800

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -3187,7 +3187,7 @@ if authentication_status:
                         mf=True
                         load_mf_number_issued=False
                         if destination=="CLEARWATER-Lewiston,ID":
-                            carrier_code=st.selectbox("Carrier Code",[info[current_release_order][current_sales_order]["carrier_code"],"432602-NTG","310897-ASHLEY"],disabled=False,key=29)
+                            carrier_code=st.selectbox("Carrier Code",[info[current_release_order][current_sales_order]["carrier_code"],"310897-ASHLEY"],disabled=False,key=29)
                         else:
                             carrier_code=st.text_input("Carrier Code",info[current_release_order][current_sales_order]["carrier_code"],disabled=True,key=9)
                         if carrier_code=="123456-KBX":
@@ -4451,7 +4451,7 @@ if authentication_status:
                     mf=True
                     load_mf_number_issued=False
                     if destination=="CLEARWATER-Lewiston,ID":
-                        carrier_code=st.selectbox("Carrier Code",[info[current_release_order][current_sales_order]["carrier_code"],"432602-NTG","310897-ASHLEY"],disabled=False,key=29)
+                        carrier_code=st.selectbox("Carrier Code",[info[current_release_order][current_sales_order]["carrier_code"],"310897-ASHLEY"],disabled=False,key=29)
                     else:
                         carrier_code=st.text_input("Carrier Code",info[current_release_order][current_sales_order]["carrier_code"],disabled=True,key=9)
                     if carrier_code=="123456-KBX":
