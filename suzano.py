ece93ee7b0a36d4e697c7b0536f8242a9664e885
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-02-16 17:44:50 -0800

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -47,7 +47,13 @@ import plotly.graph_objects as go
 import pydeck as pdk
 from pandas.tseries.offsets import BDay
 import calendar
-
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+#from IPython.display import IFrame
+from reportlab.platypus import Table, TableStyle
+import pypdfium2 as pdfium
+#import streamlit_option_menu
+#from streamlit_modal import Modal
 
 #import streamlit_option_menu
 #from streamlit_modal import Modal
