7b1476e36651e1694f3d611e2b76ba6c3ca695a0
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-03-10 14:22:39 -0700

    Update 1_LABOR.py

--- pages/1_LABOR.py
+++ pages/1_LABOR.py
@@ -1,4 +1,74 @@
+import streamlit as st
+import streamlit.components.v1 as components
+#import cv2
+import numpy as np
+#from st_files_connection import FilesConnection
+import gcsfs
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+import smtplib
+import streamlit_authenticator as sa
+from google.cloud import storage
+import os
+import io
+from io import StringIO
+from io import BytesIO
+import time
+import random
+import base64
+import streamlit_authenticator as stauth
+#from camera_input_live import camera_input_live
+import pandas as pd
+import datetime
+from requests import get
+from collections import defaultdict
+import json
+import xml.etree.ElementTree as ET
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime as dt
+#from pyzbar import pyzbar
+import pickle
+import yaml
+from yaml.loader import SafeLoader
+#from streamlit_extras.dataframe_explorer import dataframe_explorer
+import math
+import plotly.express as px               #to create interactive charts
+import plotly.graph_objects as go         #to create interactive charts
+import zipfile
+import requests
+from bs4 import BeautifulSoup
+from PIL import Image
+import plotly.graph_objects as go
+import re
+import tempfile
+import plotly.graph_objects as go
+import pydeck as pdk
+from pandas.tseries.offsets import BDay
+import calendar
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+#from IPython.display import IFrame
+from reportlab.platypus import Table, TableStyle
+import pypdfium2 as pdfium
+#import streamlit_option_menu
+#from streamlit_modal import Modal
 
+from helper import gcp_download
+from helper import gcp_download_x
+from helper import gcp_csv_to_df
+from helper import list_cs_files_f
+from helper import list_files_in_folder
+from helper import list_files_in_subfolder
+from helper import get_weather
+from helper import get_gov_weather
+
+
+#import streamlit_option_menu
+#from streamlit_modal import Modal
+
+st.set_page_config(layout="wide")
 
 labor_issue=False
 secondary=True
