1b3c92cc5bab7b4a94bb94cd4880dea8ed17a9e4
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-03-23 14:53:36 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -749,6 +749,7 @@ if authentication_status:
                         #schedule.set_index("Destination",drop=True,inplace=True)
                         schedule_frame=pd.DataFrame(schedule)
                         a=st.data_editor(schedule_frame)
+                        st.write(a.to_dict())
                         if st.button("UPDATE TABLE"):
                             storage_client = storage.Client()
                             bucket = storage_client.bucket(target_bucket)
