57ebe2576514e719013519526376080b49dae3e7
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-03-24 08:00:57 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -1223,7 +1223,7 @@ if authentication_status:
                         schedule_frame.loc[(schedule_frame['Release Order']==i[0])&(schedule_frame['Sales Order']==i[1]),"Loaded"]+=1
                     yeni=[]
                     for i in schedule_frame.index:
-                        if i!="Container":
+                        if i!="Containers":
                             st.write(i)
                             yeni.append(release_order_database[schedule_frame.loc[i,"Release Order"]][schedule_frame.loc[i,"Sales Order"]]['unitized'])
                     st.write(yeni)
