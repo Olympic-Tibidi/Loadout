fce69c23d01bdb91d07d1876934f34d3f2e09b20
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-03-31 19:55:57 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -1374,11 +1374,11 @@ if authentication_status:
                         yuk=dfb[(dfb['release_order']==rol)&(dfb['sales_order']==sale)].shape[0]
                         scheduled.loc[i,"Loaded"]=yuk
                     scheduled["Remaining"]=scheduled["Scheduled"]-scheduled["Loaded"]
-                    for col in ["Scheduled", "Loaded", "Remaining"]:
-                        scheduled[col] = pd.to_numeric(scheduled[col], errors='coerce').fillna(0).astype(int)
+                    
                     scheduled.set_index('Destination',drop=True,inplace=True)
                     scheduled.loc["Total",["Scheduled","Loaded","Remaining"]]=scheduled[["Scheduled","Loaded","Remaining"]].sum()
-                    
+                    for col in ["Scheduled", "Loaded", "Remaining"]:
+                        scheduled[col] = pd.to_numeric(scheduled[col], errors='coerce').fillna(0).astype(int)
                     st.table(scheduled)
                   
                 
