d65d6a359bd29c1c654fe4cdf7bae469d2b6d896
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-04-12 11:55:02 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -2446,7 +2446,7 @@ if authentication_status:
                          'GSSWJUV8556C': [6475.0,5],
                          'GSSWKIR6013D': [8350.0,1],
                          'GSSWKIR6013E': [850.0,1],
-                         'GSSWLAG3142E': [572.0,0],
+                         'GSSWLAG3142E': [620.0,0],
                          'GSSWLYS10628A': [1500.0,0],
                          'GSSWLYS10628B': [8500.0,0],}
                         def extract_qt(data,ro,bol):
@@ -3700,7 +3700,7 @@ if authentication_status:
                          'GSSWJUV8556C': [6475.0,5],
                          'GSSWKIR6013D': [8350.0,1],
                          'GSSWKIR6013E': [850.0,1],
-                         'GSSWLAG3142E': [572.0,0],
+                         'GSSWLAG3142E': [620.0,0],
                          'GSSWLYS10628A': [1500.0,0],
                          'GSSWLYS10628B': [8500.0,0],}
                     def extract_qt(data,ro,bol):
