cba9296f77d234d38b6a164a2ac94fa3220c7ed2
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-04-29 14:21:26 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -2474,11 +2474,11 @@ if authentication_status:
                             for ro in set(bols[k]):
                                 a,b,c=extract_qt(raw_ro,ro,k)[0],extract_qt(raw_ro,ro,k)[1],extract_qt(raw_ro,ro,k)[2]
                                 final[k]["Allocated to ROs"]+=a
-                        final[k]["Shipped"]=inv_bill_of_ladings.groupby("ocean_bill_of_lading")[['quantity']].sum().loc[k,'quantity']
-                        
-                        final[k]["Remaining in Warehouse"]=final[k]["Fit To Ship"]-final[k]["Shipped"]
-                        final[k]["Remaining on ROs"]=final[k]["Allocated to ROs"]-final[k]["Shipped"]
-                        final[k]["Remaining After ROs"]=final[k]["Fit To Ship"]-final[k]["Allocated to ROs"]
+                                final[k]["Shipped"]=inv_bill_of_ladings.groupby("ocean_bill_of_lading")[['quantity']].sum().loc[k,'quantity']
+                                
+                                final[k]["Remaining in Warehouse"]=final[k]["Fit To Ship"]-final[k]["Shipped"]
+                                final[k]["Remaining on ROs"]=final[k]["Allocated to ROs"]-final[k]["Shipped"]
+                                final[k]["Remaining After ROs"]=final[k]["Fit To Ship"]-final[k]["Allocated to ROs"]
                         temp=pd.DataFrame(final).T
                         temp.loc["TOTAL"]=temp.sum(axis=0)
                         
