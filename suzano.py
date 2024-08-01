6725ff1b651e8d673e9295baec0123b4927fae89
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-08-01 08:07:57 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -54,8 +54,7 @@ from pandas.tseries.offsets import BDay
 import calendar
 from google.cloud import documentai
 from google.oauth2 import service_account
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
+
 
 
 credentials = service_account.Credentials.from_service_account_info(st.secrets["gcs_connections"])
