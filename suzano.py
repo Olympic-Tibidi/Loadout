6d95909de978d2551d1e5898531f5fe37a0e4594
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-10-10 15:12:05 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -1469,9 +1469,10 @@ if authentication_status:
                                     st.write(carrier_mf)
                                     if release_order_number_mf not in mf_numbers.keys():   
                                         mf_numbers[release_order_number_mf]={}
-                                    if carrier_mf not in mf_numbers[release_order_number_mf]:
-                                        #mf_numbers[release_order_number_mf]={}
-                                        mf_numbers[release_order_number_mf][carrier_mf]=[]
+                                    if mf_date_str not in mf_numbers[release_order_number_mf]:
+                                        mf_numbers[release_order_number_mf][[mf_date_str]={}
+                                    if carrier_mf not in mf_numbers[release_order_number_mf][mf_date_str]:
+                                        mf_numbers[release_order_number_mf][mf_date_str][carrier_mf]=[]
                                     mf_numbers[release_order_number_mf][mf_date_str][carrier_mf]+=input_mf_numbers
                                     mf_numbers[release_order_number_mf][mf_date_str][carrier_mf]=list(set(mf_numbers[release_order_number_mf][mf_date_str][carrier_mf]))
                                     mf_data=json.dumps(mf_numbers)
