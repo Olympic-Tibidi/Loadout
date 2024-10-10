53b0db73e6fc746d53282670deeb4f425f6da299
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-10-10 15:13:10 -0700

    Update suzano.py

--- suzano.py
+++ suzano.py
@@ -1470,7 +1470,7 @@ if authentication_status:
                                     if release_order_number_mf not in mf_numbers.keys():   
                                         mf_numbers[release_order_number_mf]={}
                                     if mf_date_str not in mf_numbers[release_order_number_mf]:
-                                        mf_numbers[release_order_number_mf][[mf_date_str]={}
+                                        mf_numbers[release_order_number_mf][mf_date_str]={}
                                     if carrier_mf not in mf_numbers[release_order_number_mf][mf_date_str]:
                                         mf_numbers[release_order_number_mf][mf_date_str][carrier_mf]=[]
                                     mf_numbers[release_order_number_mf][mf_date_str][carrier_mf]+=input_mf_numbers
